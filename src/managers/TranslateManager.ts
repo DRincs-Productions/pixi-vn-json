ea159357869d82f3ed35f97281b16880e23a033d
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-10-27 19:32:04 +0100

    generateNewTranslateFile

--- src/managers/TranslateManager.ts
+++ src/managers/TranslateManager.ts
@@ -1,3 +1,5 @@
+import { PixiVNJsonLabelStep } from "../interface";
+
 export default class TranslatorManager {
     private static _translate: (key: string) => string = (key: string) => key;
     static t<T = string | string[]>(key: T): T {
@@ -8,5 +10,32 @@ export default class TranslatorManager {
     }
     static set translate(value: (key: string) => string) {
         TranslatorManager._translate = value;
+    }
+
+    addKey(json: any, key: string[] | string) {
+        if (typeof key === "string") {
+            key = [key];
+        }
+        if (Array.isArray(key)) {
+            key.forEach((k) => {
+                if (json[k] === undefined) {
+                    json[k] = "";
+                }
+            });
+        }
+    }
+    generateNewTranslateFile(labels: PixiVNJsonLabelStep[], json: object = {}) {
+        labels.forEach((label) => {
+            if (label.choices) {
+                if (Array.isArray(label.choices)) {
+                    label.choices.forEach((choice) => {
+                        if ("text" in choice) {
+                            this.addKey(json, choice.text);
+                        }
+                    });
+                }
+            }
+        });
+        return json;
     }
 }
