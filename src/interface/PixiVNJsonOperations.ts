e78c6d7124705ab3406b69fb14c5ebff2fffa84c
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-02-19 11:01:39 +0100

    Refactor import statements for consistency and improve code readability in PixiVNJsonOperations and index files

--- src/interface/PixiVNJsonOperations.ts
+++ src/interface/PixiVNJsonOperations.ts
@@ -1,17 +1,17 @@
-import PixiVNJsonCanvas from "./PixiVNJsonCanvas"
-import PixiVNJsonConditionalStatements from "./PixiVNJsonConditionalStatements"
-import PixiVNJsonIfElse from "./PixiVNJsonIfElse"
-import PixiVNJsonInput from "./PixiVNJsonInput"
-import PixiVNJsonSound from "./PixiVNJsonSound"
-import { PixiVNJsonValueGet, PixiVNJsonValueSet } from "./PixiVNJsonValue"
+import PixiVNJsonCanvas from "./PixiVNJsonCanvas";
+import PixiVNJsonConditionalStatements from "./PixiVNJsonConditionalStatements";
+import PixiVNJsonIfElse from "./PixiVNJsonIfElse";
+import PixiVNJsonInput from "./PixiVNJsonInput";
+import PixiVNJsonSound from "./PixiVNJsonSound";
+import { PixiVNJsonValueGet, PixiVNJsonValueSet } from "./PixiVNJsonValue";
 
 export type PixiVNJsonOperationString = {
-    type: "operationtoconvert",
-    values: (string | PixiVNJsonValueGet | PixiVNJsonConditionalStatements<string | PixiVNJsonValueGet>)[]
-}
+    type: "operationtoconvert";
+    values: (string | PixiVNJsonValueGet | PixiVNJsonConditionalStatements<string | PixiVNJsonValueGet>)[];
+};
 
-export type PixiVNJsonOperation = PixiVNJsonValueSet | PixiVNJsonCanvas | PixiVNJsonSound | PixiVNJsonInput
+export type PixiVNJsonOperation = PixiVNJsonValueSet | PixiVNJsonCanvas | PixiVNJsonSound | PixiVNJsonInput;
 
-type PixiVNJsonOperations = (PixiVNJsonOperation | PixiVNJsonIfElse<PixiVNJsonOperation> | PixiVNJsonOperationString)[]
+type PixiVNJsonOperations = (PixiVNJsonOperation | PixiVNJsonIfElse<PixiVNJsonOperation> | PixiVNJsonOperationString)[];
 
-export default PixiVNJsonOperations
+export default PixiVNJsonOperations;
--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,19 +1,50 @@
-export type { default as PixiVNJson } from './PixiVNJson';
-export type { default as PixiVNJsonArithmeticOperations, PixiVNJsonArithmeticOperationsArithmetic, PixiVNJsonArithmeticOperationsArithmeticSingle } from './PixiVNJsonArithmeticOperations';
-export type { default as PixiVNJsonCanvas, PixiVNJsonCanvasEdit, PixiVNJsonCanvasRemove, PixiVNJsonCanvasShow } from './PixiVNJsonCanvas';
-export type { default as PixiVNJsonCanvasEffect } from './PixiVNJsonCanvasEffect';
-export type { default as PixiVNJsonCanvasTicker } from './PixiVNJsonCanvasTicker';
-export type { default as PixiVNJsonConditionalResultToCombine } from './PixiVNJsonConditionalResultToCombine';
-export type { default as PixiVNJsonConditionalStatements } from './PixiVNJsonConditionalStatements';
-export type { PixiVNJsonComparation, default as PixiVNJsonConditions } from './PixiVNJsonConditions';
-export type { default as PixiVNJsonIfElse } from './PixiVNJsonIfElse';
-export type { default as PixiVNJsonInput } from './PixiVNJsonInput';
-export type { PixiVNJsonLabel, default as PixiVNJsonLabels } from './PixiVNJsonLabels';
-export type { PixiVNJsonChoice, PixiVNJsonChoices, PixiVNJsonDialog, PixiVNJsonDialogText, default as PixiVNJsonLabelStep, PixiVNJsonLabelToOpen } from './PixiVNJsonLabelStep';
-export type { default as PixiVNJsonMediaTransiotions } from './PixiVNJsonMediaTransiotions';
-export type { PixiVNJsonOperation, default as PixiVNJsonOperations, PixiVNJsonOperationString } from './PixiVNJsonOperations';
-export type { default as PixiVNJsonSound } from './PixiVNJsonSound';
-export type { default as PixiVNJsonStepSwitch, PixiVNJsonStepSwitchElementsType, PixiVNJsonStepSwitchElementType } from './PixiVNJsonStepSwitch';
-export type { default as PixiVNJsonUnionCondition } from './PixiVNJsonUnionCondition';
-export type { PixiVNJsonChoiceGet, PixiVNJsonLabelGet, PixiVNJsonLogicGet, PixiVNJsonParamGet, PixiVNJsonStorageGet, PixiVNJsonValueGet, PixiVNJsonValueSet } from './PixiVNJsonValue';
-
+export type { default as PixiVNJson } from "./PixiVNJson";
+export type {
+    default as PixiVNJsonArithmeticOperations,
+    PixiVNJsonArithmeticOperationsArithmetic,
+    PixiVNJsonArithmeticOperationsArithmeticSingle,
+} from "./PixiVNJsonArithmeticOperations";
+export type {
+    default as PixiVNJsonCanvas,
+    PixiVNJsonCanvasEdit,
+    PixiVNJsonCanvasRemove,
+    PixiVNJsonCanvasShow,
+} from "./PixiVNJsonCanvas";
+export type { default as PixiVNJsonCanvasEffect } from "./PixiVNJsonCanvasEffect";
+export type { default as PixiVNJsonCanvasTicker } from "./PixiVNJsonCanvasTicker";
+export type { default as PixiVNJsonConditionalResultToCombine } from "./PixiVNJsonConditionalResultToCombine";
+export type { default as PixiVNJsonConditionalStatements } from "./PixiVNJsonConditionalStatements";
+export type { PixiVNJsonComparation, default as PixiVNJsonConditions } from "./PixiVNJsonConditions";
+export type { default as PixiVNJsonIfElse } from "./PixiVNJsonIfElse";
+export type { default as PixiVNJsonInput } from "./PixiVNJsonInput";
+export type { PixiVNJsonLabel, default as PixiVNJsonLabels } from "./PixiVNJsonLabels";
+export type {
+    PixiVNJsonChoice,
+    PixiVNJsonChoices,
+    PixiVNJsonDialog,
+    PixiVNJsonDialogText,
+    default as PixiVNJsonLabelStep,
+    PixiVNJsonLabelToOpen,
+} from "./PixiVNJsonLabelStep";
+export type { default as PixiVNJsonMediaTransiotions } from "./PixiVNJsonMediaTransiotions";
+export type {
+    PixiVNJsonOperation,
+    default as PixiVNJsonOperations,
+    PixiVNJsonOperationString,
+} from "./PixiVNJsonOperations";
+export type { default as PixiVNJsonSound } from "./PixiVNJsonSound";
+export type {
+    default as PixiVNJsonStepSwitch,
+    PixiVNJsonStepSwitchElementsType,
+    PixiVNJsonStepSwitchElementType,
+} from "./PixiVNJsonStepSwitch";
+export type { default as PixiVNJsonUnionCondition } from "./PixiVNJsonUnionCondition";
+export type {
+    PixiVNJsonChoiceGet,
+    PixiVNJsonLabelGet,
+    PixiVNJsonLogicGet,
+    PixiVNJsonParamGet,
+    PixiVNJsonStorageGet,
+    PixiVNJsonValueGet,
+    PixiVNJsonValueSet,
+} from "./PixiVNJsonValue";
