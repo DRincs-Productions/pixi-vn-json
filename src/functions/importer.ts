46b99a09fd1b3de078c5932fda4c75bd6c64b516
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-02-27 23:06:11 +0100

    Update peer dependency version and refactor import statements for improved clarity and consistency in importer function

--- src/functions/importer.ts
+++ src/functions/importer.ts
@@ -1,8 +1,8 @@
-import { saveLabel, StorageManagerStatic } from "@drincs/pixi-vn"
-import { LabelJson } from "../classes"
-import { LabelJsonOptions } from "../classes/LabelJson"
-import { PixiVNJson, PixiVNJsonLabelStep } from "../interface"
-import { runOperation } from "./operation-utility"
+import { saveLabel, storage, StorageElementType } from "@drincs/pixi-vn";
+import { LabelJson } from "../classes";
+import { LabelJsonOptions } from "../classes/LabelJson";
+import { PixiVNJson, PixiVNJsonLabelStep } from "../interface";
+import { runOperation } from "./operation-utility";
 
 /**
  * Import a Pixi'VN JSON to the system.
@@ -10,40 +10,44 @@ import { runOperation } from "./operation-utility"
  * @param data The Pixi'VN JSON to be imported
  * @returns
  */
-export function importPixiVNJson(
-    data: PixiVNJson | string,
-    options: LabelJsonOptions = {}
-) {
-    let operationStringConvert = options?.operationStringConvert
+export function importPixiVNJson(data: PixiVNJson | string, options: LabelJsonOptions = {}) {
+    let operationStringConvert = options?.operationStringConvert;
     try {
         if (typeof data === "string") {
-            data = JSON.parse(data) as PixiVNJson
+            data = JSON.parse(data) as PixiVNJson;
         }
-    }
-    catch (e) {
-        console.error("[Pixi’VN Json] Error parsing imported Pixi'VN JSON", e)
-        return
+    } catch (e) {
+        console.error("[Pixi’VN Json] Error parsing imported Pixi'VN JSON", e);
+        return;
     }
     if (typeof data !== "object") {
-        console.error("[Pixi’VN Json] Error parsing imported Pixi'VN JSON: data is not an object")
-        return
+        console.error("[Pixi’VN Json] Error parsing imported Pixi'VN JSON: data is not an object");
+        return;
     }
     if (data.initialOperations) {
         for (let operation of data.initialOperations) {
-            runOperation(operation, operationStringConvert ? (value) => operationStringConvert(value, {}, {}) : undefined)
+            runOperation(
+                operation,
+                operationStringConvert ? (value) => operationStringConvert(value, {}, {}) : undefined
+            );
         }
-        StorageManagerStatic.saveStorageAsBasicStorage()
+        let basicStorage: {
+            [key: string]: StorageElementType;
+        } = {};
+        [...storage.storage.keys()].forEach((value, key) => {
+            basicStorage[value] = storage.storage.get(value);
+        });
+        storage.startingStorage = basicStorage;
     }
     if (data.labels) {
-        let labels = data.labels
+        let labels = data.labels;
         for (const labelId in labels) {
             try {
-                const steps: PixiVNJsonLabelStep[] = labels[labelId]
-                let label = new LabelJson(labelId, steps, undefined, options)
-                saveLabel(label)
-            }
-            catch (e) {
-                console.error(`[Pixi’VN Json] Error creating JSON label ${labelId}`, e)
+                const steps: PixiVNJsonLabelStep[] = labels[labelId];
+                let label = new LabelJson(labelId, steps, undefined, options);
+                saveLabel(label);
+            } catch (e) {
+                console.error(`[Pixi’VN Json] Error creating JSON label ${labelId}`, e);
             }
         }
     }
