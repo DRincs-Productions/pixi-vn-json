bfe2be6e5983ec736c78dedefbfb087b7c73719c
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-01-02 18:33:43 +0100

    Refactor test setup by removing unnecessary test wrapper for initialization

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,16 +1,13 @@
 import { Game } from "@drincs/pixi-vn";
 import { JsonUnifier } from "@drincs/pixi-vn-json/unifier";
-import { test } from "vitest";
 import { narrationOperation } from "../src/utils/narration";
 import { getConditionalStep, getLogichValue, setInitialStorageValue, setStorageValue } from "../src/utils/storage";
 
-test("setup", async () => {
-    Game.init();
-    JsonUnifier.init({
-        narrationOperation: narrationOperation,
-        setStorageValue: setStorageValue,
-        setInitialStorageValue: setInitialStorageValue,
-        getLogichValue: getLogichValue,
-        getConditionalStep: getConditionalStep,
-    });
+Game.init();
+JsonUnifier.init({
+    narrationOperation: narrationOperation,
+    setStorageValue: setStorageValue,
+    setInitialStorageValue: setInitialStorageValue,
+    getLogichValue: getLogichValue,
+    getConditionalStep: getConditionalStep,
 });
