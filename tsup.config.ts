4343361abce0c6c7e8aa25c7a53950fc31d0f838
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-03-31 22:34:05 +0200

    Update version to 1.11.1 and refactor tsup configuration for improved readability and maintainability

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,86 +1,90 @@
-import { defineConfig } from "tsup";
+import { defineConfig, type Options } from "tsup";
 
-export default defineConfig([
-    {
-        target: "es2020",
-        entry: {
-            core: "src/core/index.ts",
-        },
-        format: ["cjs", "esm"],
-        dts: true,
-        treeshake: true,
-        splitting: false,
-        sourcemap: true,
-        clean: false,
-        minify: true,
-        bundle: true,
-        skipNodeModulesBundle: false,
-        outExtension({ format }) {
-            return {
-                js: format === "esm" ? ".mjs" : ".cjs",
-            };
-        },
-    },
-    {
-        target: "es2020",
-        entry: {
-            translator: "src/translator/index.ts",
-        },
-        format: ["cjs", "esm"],
-        dts: true,
-        treeshake: true,
-        splitting: false,
-        sourcemap: true,
-        clean: false,
-        minify: true,
-        bundle: true,
-        skipNodeModulesBundle: false,
-        external: ["@drincs/pixi-vn-json/core"],
-        outExtension({ format }) {
-            return {
-                js: format === "esm" ? ".mjs" : ".cjs",
-            };
-        },
-    },
-    {
-        target: "es2020",
-        entry: {
-            importer: "src/importer/index.ts",
-        },
-        format: ["cjs", "esm"],
-        dts: true,
-        treeshake: true,
-        splitting: false,
-        sourcemap: true,
-        clean: false,
-        minify: true,
-        bundle: true,
-        skipNodeModulesBundle: false,
-        external: ["@drincs/pixi-vn-json/core", "@drincs/pixi-vn-json/translator"],
-        outExtension({ format }) {
-            return {
-                js: format === "esm" ? ".mjs" : ".cjs",
-            };
-        },
-    },
-    {
-        target: "es2020",
-        entry: {
-            index: "src/index.ts",
-        },
-        format: ["cjs", "esm"], // Build for commonJS and ESmodules
-        dts: true, // Generate declaration file (.d.ts)
-        treeshake: true,
-        // sourcemap: true, // Generate sourcemap, it was removed because otherwise it would explode
-        clean: false,
-        minify: true,
-        bundle: true,
-        skipNodeModulesBundle: false, // Skip bundling of node_modules
-        external: ["@drincs/pixi-vn-json/core", "@drincs/pixi-vn-json/importer", "@drincs/pixi-vn-json/translator"],
-        outExtension({ format }) {
-            return {
-                js: format === "esm" ? ".mjs" : ".cjs",
-            };
-        },
-    },
-]);
+export default defineConfig((options) => {
+    const sourcemap = Boolean(options.watch);
+    const createConfig = (config: Options): Options => ({
+        sourcemap,
+        ...config,
+    });
+
+    return [
+        createConfig({
+            target: "es2020",
+            entry: {
+                core: "src/core/index.ts",
+            },
+            format: ["cjs", "esm"],
+            dts: true,
+            treeshake: true,
+            splitting: false,
+            clean: false,
+            minify: true,
+            bundle: true,
+            skipNodeModulesBundle: false,
+            outExtension({ format }) {
+                return {
+                    js: format === "esm" ? ".mjs" : ".cjs",
+                };
+            },
+        }),
+        createConfig({
+            target: "es2020",
+            entry: {
+                translator: "src/translator/index.ts",
+            },
+            format: ["cjs", "esm"],
+            dts: true,
+            treeshake: true,
+            splitting: false,
+            clean: false,
+            minify: true,
+            bundle: true,
+            skipNodeModulesBundle: false,
+            external: ["@drincs/pixi-vn-json/core"],
+            outExtension({ format }) {
+                return {
+                    js: format === "esm" ? ".mjs" : ".cjs",
+                };
+            },
+        }),
+        createConfig({
+            target: "es2020",
+            entry: {
+                importer: "src/importer/index.ts",
+            },
+            format: ["cjs", "esm"],
+            dts: true,
+            treeshake: true,
+            splitting: false,
+            clean: false,
+            minify: true,
+            bundle: true,
+            skipNodeModulesBundle: false,
+            external: ["@drincs/pixi-vn-json/core", "@drincs/pixi-vn-json/translator"],
+            outExtension({ format }) {
+                return {
+                    js: format === "esm" ? ".mjs" : ".cjs",
+                };
+            },
+        }),
+        createConfig({
+            target: "es2020",
+            entry: {
+                index: "src/index.ts",
+            },
+            format: ["cjs", "esm"], // Build for commonJS and ESmodules
+            dts: true, // Generate declaration file (.d.ts)
+            treeshake: true,
+            clean: false,
+            minify: true,
+            bundle: true,
+            skipNodeModulesBundle: false, // Skip bundling of node_modules
+            external: ["@drincs/pixi-vn-json/core", "@drincs/pixi-vn-json/importer", "@drincs/pixi-vn-json/translator"],
+            outExtension({ format }) {
+                return {
+                    js: format === "esm" ? ".mjs" : ".cjs",
+                };
+            },
+        }),
+    ];
+});
