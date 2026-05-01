4d49023a49fe5bb8f6bd3198c15b71080b81fa4f
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-05-01 15:39:44 +0200

    refactor: update PixiVNJsonComparationOperatorsType and related exports for consistency

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,12 @@
+import type { PixiVNJsonComparationOperatorsType } from "@/interface/PixiVNJsonConditions";
+
 export const PIXIVNJSON_PARAM_ID = "___param___";
+export const PixiVNJsonComparationOperators: PixiVNJsonComparationOperatorsType[] = [
+    "==",
+    "!=",
+    "<",
+    "<=",
+    ">",
+    ">=",
+    "CONTAINS",
+];
