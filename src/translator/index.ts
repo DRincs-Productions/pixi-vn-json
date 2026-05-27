e1e6f0b455cebcdf79974830ed835c0c13fae84b
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-05-27 09:07:14 +0200

    improve

--- src/translator/index.ts
+++ src/translator/index.ts
@@ -1,7 +1,3 @@
-import TranslateManager from "./TranslatorManager";
-
-const translator = TranslateManager;
-
-export { translator };
 export type * from "@/translator/interfaces/ReplaceHandler";
 export * from "@/translator/TextReplaces";
+export { default as translator } from "./TranslatorManager";
